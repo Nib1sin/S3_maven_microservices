be1433360e2aedcd6a8d8adf98cf6d6e24c650a4
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-26 17:54:19 +0200

    NEW - Agregando microservice Config

---
+++ microservice-conifg/src/main/java/com/efosft/microservice/conifg/MicroserviceConifgApplication.java
@@ -0,0 +1,13 @@
+package com.efosft.microservice.conifg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConifgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceConifgApplication.class, args);
+	}
+
+}
---
+++ microservice-conifg/src/test/java/com/efosft/microservice/conifg/MicroserviceConifgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efosft.microservice.conifg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceConifgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
