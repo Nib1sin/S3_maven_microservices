f1ffdc45ec7d6484e64e49bbc8e2a18bc67b1cbd
Nib1sin <38408560+Nib1sin@users.noreply.github.com>
2024-06-27 03:42:16 +0200

    ENH - Configurando microservice Gateway

--- microservice-gateway/src/main/java/com/efosft/microservice/gateway/MicroserviceGatewayApplication.java
+++ microservice-gateway/src/main/java/com/efosft/microservice/gateway/MicroserviceGatewayApplication.java
@@ -2,6 +2,8 @@ package com.efosft.microservice.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 @SpringBootApplication
 public class MicroserviceGatewayApplication {
